942275b0e60f0e7dbbbb1da611f7e5774e1eff53
Gugulothu Mukesh Varma <mukeshvarma.g@gmail.com>
2023-04-27 18:04:04 +0530

    Added Task File

---
+++ src/test/java/com/SocGen/Task/TaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SocGen.Task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
